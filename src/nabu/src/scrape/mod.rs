8db750dcbb47dcb22e6f52deeef6ce3eccf399b5
decentman <decentboi6969@gmail.com>
2021-08-25 15:00:55 +0530

    Implemented User-Agent Pooling for browser anonymity(using 'fastrand')

--- src/nabu/src/scrape/mod.rs
+++ src/nabu/src/scrape/mod.rs
@@ -13,17 +13,32 @@
 
 use crate::orel;
 use std::time::Duration;
+use fastrand;
 use ureq::{Agent, AgentBuilder, Error};
 use select::document::Document;
 use select::predicate::{Attr, Class, Name, Predicate};
 //use serde_json;
 mod types;
 
+const WAIT_FOR_RESPONSE_TIMEOUT : u64 = 10;
+const USER_AGENT_POOL : [&'static str; 11] = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.3 Safari/605.1.15",
+                                              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+                                              "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+                                              "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+                                              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+                                              "Mozilla/5.0 (Windows NT 10.0; Trident/7.0; rv:11.0) like Gecko",
+                                              "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
+                                              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.78",
+                                              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Vivaldi/4.1",
+                                              "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Vivaldi/4.1",
+                                              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Vivaldi/4.1",
+                                            ];
+
 pub fn make_request(url: &str) -> Result<String,ureq::Error>{
     println!("Making Request!");
     let n_agent: Agent = AgentBuilder::new()
-                              .timeout(Duration::from_secs(5))
-                              .user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.3 Safari/605.1.15")
+                              .timeout(Duration::from_secs(WAIT_FOR_RESPONSE_TIMEOUT))
+                              .user_agent(USER_AGENT_POOL[fastrand::usize(..USER_AGENT_POOL.len())])
                               .build();
     let http_response: String = n_agent.get(url)
                                        .set("ACCEPT","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
@@ -46,8 +61,9 @@ pub fn make_request(url: &str) -> Result<String,ureq::Error>{
 pub fn stage_one(html_response: &str, website_profile: orel::Orel<String>) -> types::Listing<String> {
     //println!("{:#?}",html_response); // VERBOSE
     //panic!("--BREAKPOINT--");
+    println!("Parsing..");
     let html_document = Document::from(html_response);
-    let mut plisting: types::Listing<String>;
+    let mut plisting: types::Listing<String> = Default::default();
     if website_profile.listing_find_by == "Class"
     {   for lnode in html_document.find(Class(&website_profile.listing_identifier[..])) {
             //println!("Inside for loop");
@@ -100,7 +116,7 @@ pub fn stage_one(html_response: &str, website_profile: orel::Orel<String>) -> ty
 }
 
 pub fn stage_two(part_listing: types::Listing<String>) {
-    //do lv2 here
+    //do lv2 stuff here
 }
 
 #[test]
