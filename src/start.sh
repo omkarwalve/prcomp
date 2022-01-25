bc5a1c790a837df3bb0215d3ccf4a5bb7b41881c
decentman <decentboi6969@gmail.com>
2022-01-25 23:33:06 +0530

    New tmux startup script to setup development environment for frontend and backend

---
+++ src/start.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+SESSION="KILOWOG"
+WINDOW=0
+
+tmux new-session -d -s $SESSION
+tmux rename-window -t $SESSION:$WINDOW 'kwe'
+tmux send-keys -t $SESSION:$WINDOW 'cd kwe/ && cargo run --release' C-m
+tmux split-window -h -t $SESSION:$WINDOW 'cd www/ && npm start'
+tmux attach-session -t $SESSION
